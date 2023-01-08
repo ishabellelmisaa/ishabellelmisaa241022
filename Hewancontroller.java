0bd57ca837001da85c3e7a36d35095e924f90351
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:58:24 +0700

    Create Hewancontroller.java

---
+++ Hewancontroller.java
@@ -0,0 +1,25 @@
+public class Hewancontroller {
+	private Hewan model; 
+	private HewanView view; 
+	public Hewancontroller(Hewan model, HewanView view){ 
+
+            this.model = model; 
+		this.view = view; 
+	} 
+	public void setHewanJenis(String jenis){ 
+		model.setJenis(jenis); 
+	} 
+	public String getHewanJenis(){ 
+		return model.getJenis(); 
+	} 
+	public void setHewanName(String name){ 
+		model.setName(name); 
+	} 
+	public String getHewanName(){ 
+		return model.getName(); 
+	} 
+	public void updateView(){ 
+		view.printHewanDetails(model.getJenis(), model.getName()); 
+	} 
+
+}
