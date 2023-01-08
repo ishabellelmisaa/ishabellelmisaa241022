60078171f4d0107436a4d00996fa646592059672
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:57:54 +0700

    Create HewanView.java

---
+++ HewanView.java
@@ -0,0 +1,7 @@
+public class HewanView { 
+	public void printHewanDetails(String hewanName, String hewanJenis){ 
+		System.out.println("Student Detail: "); 
+		System.out.println("Nama Hewan: " + hewanJenis); 
+		System.out.println("Jenis Hewan : " + hewanName); 
+	} 
+}
