f3a546bd59c1c339552c83bcc0c372d2959655ab
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:57:30 +0700

    Create Hewan.java

---
+++ Hewan.java
@@ -0,0 +1,18 @@
+class Hewan { 
+
+	private String name; 
+	private String jenis; 
+
+	public String getName() { 
+		return name; 
+	} 
+	public void setName(String name) { 
+		this.name = name; 
+	} 
+	public String getJenis() { 
+		return jenis; 
+	} 
+	public void setJenis(String jenis) { 
+		this.jenis = jenis; 
+	} 
+}
